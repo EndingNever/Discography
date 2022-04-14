112fc99986de14998103307c5d3dd46a3de3d6c7
Fabian Villasenor <exionite@gmail.com>
2022-04-14 15:19:55 -0700

    artist

--- src/components/Discography/Discography.js
+++ src/components/Discography/Discography.js
@@ -6,27 +6,43 @@ import albums from './DiscoData'
 
 
 export default function Discography() {
-  const handleHover = () =>{
+  const handleHover = () => {
 
   }
 
   const albumMap = albums.map((album) => (
-    
-      <div className='discography-container'>
-        <div className='album-container'>
-          <img className='album-art' src={album.albumArt} alt="" />
-          <p className='album-name'>
-            {album.albumName}
-          </p>
-          <p className='album-artist'>
-            {album.artist}
-          </p>
-        </div>
+    <div className='discography-container'>
+      <div className='album-container'>
+        <img className='album-art' src={album.albumArt} alt="" />
+        <p className='album-name'>
+          {album.albumName}
+        </p>
+        <p className='album-artist'>
+          {album.artist}
+        </p>
       </div>
+    </div>
+  ));
+
+  const artistMap = albums.map((album) => (
+    <option value={album.artist}>{album.artist}</option>
   ))
+
 
   return (
     <>
+      <div>
+        <select name="genre" id="genre">
+          {artistMap}
+        </select>
+        <select name="artist" id="artist">
+          {albumMap}
+          <option value="Artist" selected>Artist</option>
+          <option value></option>
+          <option value="reggae">Reggae</option>
+          <option value="hip hop">Hip Hop</option>
+        </select>
+      </div>
       {albumMap}
     </>
   )
